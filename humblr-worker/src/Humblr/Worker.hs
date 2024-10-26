5f2c1026eddcf1cc0f0a543c94378364ca93a6bf
Hiromi ISHII <konn.jinro@gmail.com>
2024-10-26 21:35:23 +0900

    Revert "Stop preparing"
    This reverts commit c9f837443d6f72e47bbe2204714bf54f0a75c2a5.

--- humblr-worker/src/Humblr/Worker.hs
+++ humblr-worker/src/Humblr/Worker.hs
@@ -26,6 +26,7 @@ import Data.Aeson qualified as J
 import Data.ByteString.Char8 qualified as BS8
 import Data.Coerce (coerce)
 import Data.Either (partitionEithers)
+import Data.Functor ((<&>))
 import Data.Maybe (fromMaybe)
 import Data.String (fromString)
 import Data.Text qualified as T
@@ -160,42 +161,49 @@ adminAPI usr =
 
 putArticle :: AuthResult User -> T.Text -> ArticleUpdate -> Handler HumblrEnv NoContent
 putArticle user slug upd = protectIfConfigured user do
+  tryInsertTagQ <- mkTryInsertTagQ
+  lookupTagQ <- mkLookupTagNameQ
+  tagArticleQ <- mkTagArticleQ
+  lookSlugQ <- mkLookupSlugQ
   art <-
     either (\e -> serverError err500 {errBody = "Schema Error (article): " <> BS8.pack e}) (pure . (.id))
       . D1.parseD1RowView @ArticleRow
       =<< maybe (serverError err404 {errBody = "Article not found: " <> TE.encodeUtf8 slug}) (pure)
       =<< liftIO . (wait <=< D1.first)
-      =<< lookupSlugQ slug
+      =<< bind lookSlugQ slug
   -- FIXME: make these two together with @art@ into a single batch.
-  let tags = V.fromList upd.tags
-  insTagsQ <- tryInsertTagQ tags
-  tagIdsQ <- lookupTagsQ tags
+  insTagsQ <- mapM (bind tryInsertTagQ) upd.tags
+  tagIdsQ <- mapM (bind lookupTagQ) upd.tags
   d1 <- getBinding "D1"
   tagIds <-
-    V.mapMaybe (either (const Nothing) (Just . (.id)) . D1.parseD1RowView @TagRow)
+    V.toList
+      . V.mapMaybe (either (const Nothing) (Just . (.id)) . D1.parseD1RowView @TagRow)
       . V.concatMap (.results)
-      <$> liftIO (wait =<< D1.batch d1 (insTagsQ <> tagIdsQ))
+      <$> liftIO (wait =<< D1.batch d1 (V.fromList $ insTagsQ ++ tagIdsQ))
 
-  updQ <- articleUpdateBodyQ art upd
-  delTagQ <- deleteAllTagsOnArticleQ art
-  insTagQ <- tagArticleQ tagIds art
+  updQ <- mkArticleUpdateBody >>= \q -> bind q art upd
+  delTagQ <- mkDeleteAllTagsOnArticle >>= \q -> bind q art
+  insTagQ <- mapM (bind tagArticleQ art) tagIds
   resl <-
     liftIO $
       wait
-        =<< D1.batch d1 (updQ `V.cons` (delTagQ `V.cons` insTagQ))
+        =<< D1.batch d1 (V.fromList $ updQ : delTagQ : insTagQ)
   unless (all (.success) resl) $
     serverError err500 {errBody = "Failed to update article"}
   pure NoContent
 
 deleteArticle :: AuthResult User -> T.Text -> Handler HumblrEnv NoContent
 deleteArticle user slug = protectIfConfigured user do
-  met <- liftIO . (wait <=< D1.first) =<< lookupSlugQ slug
+  lookupFromSlug <- mkLookupSlugQ
+  met <- liftIO . (wait <=< D1.first) =<< bind lookupFromSlug slug
   let martId = either (const Nothing) (Just . (.id)) . D1.parseD1RowView @ArticleRow =<< met
   case martId of
     Nothing -> serverError err404 {errBody = "Article not found"}
     Just aid -> do
-      delTags <- deleteAllTagsOnArticleQ aid
-      delArt <- deleteArticleQ aid
+      delQ <- mkDeleteAllTagsOnArticle
+      delTags <- bind delQ aid
+      delArtQ <- mkDeleteArticle
+      delArt <- bind delArtQ aid
       d1 <- getBinding "D1"
       NoContent <$ liftIO (wait =<< D1.batch d1 (V.fromList [delTags, delArt]))
 
@@ -244,13 +252,23 @@ data ArticleRow = ArticleRow
   deriving (Show, Eq, Ord, Generic)
   deriving anyclass (FromD1Row, ToD1Row)
 
+type family xs ~> a where
+  '[] ~> a = a
+  (x ': xs) ~> a = x -> (xs ~> a)
+
+newtype Preparation params = Preparation (params ~> App D1.Statement)
+
+bind :: forall params. Preparation params -> params ~> App D1.Statement
+bind (Preparation f) = f
+
 getRecentArticles ::
   Maybe Word32 ->
   App [Article]
 getRecentArticles page = do
+  recents <- mkRecentArticlesQ
   rows <-
     liftIO . (wait <=< D1.all)
-      =<< recentArticlesQ (maybe 0 (* 10) page)
+      =<< bind recents (maybe 0 (* 10) page)
   unless rows.success $
     throwString "Failed to fetch recent articles"
   let (fails, articles) = partitionEithers $ map D1.parseD1RowView $ V.toList rows.results
@@ -259,29 +277,83 @@ getRecentArticles page = do
       "Failed to parse article row: " <> show fails
   mapM fromArticleRow articles
 
+mkRecentArticlesQ :: App (Preparation '[Word32])
+mkRecentArticlesQ = do
+  d1 <- getBinding "D1"
+  liftIO (D1.prepare d1 "SELECT * FROM articles ORDER BY createdAt DESC LIMIT 10 OFFSET ?") <&> \prep ->
+    Preparation \page ->
+      liftIO $ D1.bind prep (V.singleton $ D1.toD1ValueView $ page * 10)
+
+mkLookupSlugQ :: App (Preparation '[T.Text])
+mkLookupSlugQ = do
+  d1 <- getBinding "D1"
+  liftIO $
+    D1.prepare d1 "SELECT * FROM articles WHERE slug = ?" <&> \prep ->
+      Preparation \slug ->
+        liftIO $ D1.bind prep (V.singleton $ D1.toD1ValueView slug)
+
+mkArticleUpdateBody :: App (Preparation '[ArticleId, ArticleUpdate])
+mkArticleUpdateBody = do
+  prepare "UPDATE articles SET body = ?1, lastUpdate = ?2 WHERE id = ?3" <&> \prep ->
+    Preparation \aid ArticleUpdate {..} -> do
+      now <- liftIO getCurrentTime
+      liftIO $
+        D1.bind prep $
+          V.fromList
+            [ D1.toD1ValueView body
+            , D1.toD1ValueView now
+            , D1.toD1ValueView aid
+            ]
+
 lookupSlug :: T.Text -> App (Maybe Article)
 lookupSlug slug = do
-  mrow <- liftIO . (wait <=< D1.first) =<< lookupSlugQ slug
+  lookupQ <- mkLookupSlugQ
+  mrow <- liftIO . (wait <=< D1.first) =<< bind lookupQ slug
   forM mrow $ \row -> do
     case D1.parseD1RowView row of
       Right r -> fromArticleRow r
       Left err -> throwString err
 
+prepare :: String -> App D1.PreparedStatement
+prepare q = do
+  d1 <- getBinding "D1"
+  liftIO $ D1.prepare d1 q
+
+mkTryInsertTagQ :: App (Preparation '[T.Text])
+mkTryInsertTagQ =
+  prepare "INSERT OR IGNORE INTO tags (name) VALUES (?)"
+    <&> \prep -> Preparation \name ->
+      liftIO $ D1.bind prep (V.singleton $ D1.toD1ValueView name)
+
+mkLookupTagNameQ :: App (Preparation '[T.Text])
+mkLookupTagNameQ =
+  prepare "SELECT * FROM tags WHERE name = ?" <&> \prep ->
+    Preparation \name ->
+      liftIO $ D1.bind prep (V.singleton $ D1.toD1ValueView name)
+
+mkArticleTagsQ :: App (Preparation '[ArticleId])
+mkArticleTagsQ =
+  prepare "SELECT tag.name FROM tags tag INNER JOIN articleTags assoc ON tag.id = assoc.tag WHERE assoc.article = ?" <&> \prep ->
+    Preparation \aid ->
+      liftIO $ D1.bind prep (V.singleton $ D1.toD1ValueView aid)
+
 createArticle :: ArticleSeed -> App ()
-createArticle ArticleSeed {tags = ts0, ..} = do
-  let tags = V.fromList ts0
-  tagQs <- tryInsertTagQ tags
-  newArt <- insertArticleQ ArticleSeed {tags = ts0, ..}
-  tagIdsQ <- lookupTagsQ tags
+createArticle ArticleSeed {..} = do
+  tryInsertQ <- mkTryInsertTagQ
+  insertArtQ <- mkInsertArticleQ
+  lookupTagQ <- mkLookupTagNameQ
+  tagArtQ <- mkTagArticleQ
+  tagQs <- mapM (bind tryInsertQ) tags
+  newArt <- bind insertArtQ ArticleSeed {..}
+  tagIdsQ <- mapM (bind lookupTagQ) tags
   artIdQ <-
-    prepareBind "SELECT id FROM articles WHERE slug = ?" $
-      V.singleton $
-        D1.toD1ValueView slug
+    prepare "SELECT id FROM articles WHERE slug = ?" >>= \q ->
+      liftIO (D1.bind q $ V.singleton $ D1.toD1ValueView slug)
   d1 <- getBinding "D1"
   vs <-
     liftIO $
       wait
-        =<< D1.batch d1 (newArt `V.cons` tagQs <> tagIdsQ <> V.singleton artIdQ)
+        =<< D1.batch d1 (V.fromList $ newArt : tagQs ++ tagIdsQ ++ [artIdQ])
   unless (V.all (.success) vs) $
     serverError $
       err409 {errBody = "Failed to insert article: " <> TE.encodeUtf8 slug}
@@ -292,21 +364,68 @@ createArticle ArticleSeed {tags = ts0, ..} = do
         either (error "Failed to parse article") (.id) $
           D1.parseD1RowView @ArticleIdRow $
             V.head rawArtId.results
-  tagArtQs <- tagArticleQ tagIds artId
   unless (null tagIds) $
-    liftIO $
-      void $
-        wait =<< D1.batch d1 tagArtQs
+    void $
+      liftIO . D1.batch d1
+        =<< mapM (bind tagArtQ artId) tagIds
 
 newtype ArticleIdRow = ArticleIdRow {id :: ArticleId}
   deriving (Show, Eq, Ord, Generic)
   deriving anyclass (FromD1Row)
 
+mkInsertArticleQ :: App (Preparation '[ArticleSeed])
+mkInsertArticleQ =
+  prepare "INSERT INTO articles (body, createdAt, lastUpdate, slug) VALUES (?1, ?2, ?3, ?4)" <&> \prep ->
+    Preparation \ArticleSeed {..} -> do
+      now <- liftIO getCurrentTime
+      liftIO $
+        D1.bind prep $
+          V.fromList
+            [ D1.toD1ValueView body
+            , D1.toD1ValueView now
+            , D1.toD1ValueView now
+            , D1.toD1ValueView slug
+            ]
+
+mkTagArticleQ :: App (Preparation '[ArticleId, TagId])
+mkTagArticleQ =
+  prepare "INSERT INTO articleTags (article, tag) VALUES (?1, ?2) ON CONFLICT DO NOTHING" <&> \prep ->
+    Preparation \aid tid ->
+      liftIO $
+        D1.bind prep $
+          V.fromList
+            [ D1.toD1ValueView aid
+            , D1.toD1ValueView tid
+            ]
+
+mkArticleWithTagsQ :: App (Preparation '[T.Text, Word32])
+mkArticleWithTagsQ =
+  prepare "SELECT a.* FROM articles a INNER JOIN articleTags at ON a.id = at.article INNER JOIN tags t ON at.tag = t.id WHERE t.name = ?1 ORDER BY a.createdAt DESC LIMIT 10 OFFSET ?2" <&> \prep ->
+    Preparation \name page ->
+      liftIO $ D1.bind prep (V.fromList [D1.toD1ValueView name, D1.toD1ValueView $ page * 10])
+
+mkDeleteAllTagsOnArticle :: App (Preparation '[ArticleId])
+mkDeleteAllTagsOnArticle = do
+  d1 <- getBinding "D1"
+  liftIO $
+    D1.prepare d1 "DELETE FROM articleTags WHERE article = ?" <&> \prep ->
+      Preparation \art ->
+        liftIO $ D1.bind prep (V.singleton $ D1.toD1ValueView art)
+
+mkDeleteArticle :: App (Preparation '[ArticleId])
+mkDeleteArticle = do
+  d1 <- getBinding "D1"
+  liftIO $
+    D1.prepare d1 "DELETE FROM articles WHERE id = ?" <&> \prep ->
+      Preparation \aid ->
+        liftIO $ D1.bind prep (V.singleton $ D1.toD1ValueView aid)
+
 getArticlesWithTag :: T.Text -> Maybe Word32 -> App [Article]
 getArticlesWithTag tag mpage = do
+  artsWithTagQ <- mkArticleWithTagsQ
   rows <-
     liftIO . (wait <=< D1.all)
-      =<< articleWithTagsQ tag (fromMaybe 0 mpage)
+      =<< bind artsWithTagQ tag (fromMaybe 0 mpage)
   unless rows.success $
     throwString "Failed to fetch articles with tag"
 
@@ -323,93 +442,12 @@ newtype TagName = TagName {name :: T.Text}
 getTagName :: TagName -> T.Text
 getTagName = coerce
 
-prepareBind :: String -> V.Vector D1.D1ValueView -> App D1.Statement
-prepareBind q args = do
-  d1 <- getBinding "D1"
-  liftIO $ flip D1.bind args =<< D1.prepare d1 q
-
-prepareBinds :: (Traversable t) => String -> t (V.Vector D1.D1ValueView) -> App (t D1.Statement)
-prepareBinds q args = do
-  d1 <- getBinding "D1"
-  liftIO $ D1.prepare d1 q >>= forM args . D1.bind
-
-recentArticlesQ :: Word32 -> App D1.Statement
-recentArticlesQ =
-  prepareBind "SELECT * FROM articles ORDER BY createdAt DESC LIMIT 10 OFFSET ?"
-    . V.singleton
-    . D1.toD1ValueView
-    . (* 10)
-
-lookupSlugQ :: T.Text -> App D1.Statement
-lookupSlugQ =
-  prepareBind "SELECT * FROM articles WHERE slug = ?"
-    . V.singleton
-    . D1.toD1ValueView
-
-articleUpdateBodyQ :: ArticleId -> ArticleUpdate -> App D1.Statement
-articleUpdateBodyQ aid ArticleUpdate {..} = do
-  now <- liftIO getCurrentTime
-  prepareBind "UPDATE articles SET body = ?1, lastUpdate = ?2 WHERE id = ?3" $
-    V.fromList
-      [ D1.toD1ValueView body
-      , D1.toD1ValueView now
-      , D1.toD1ValueView aid
-      ]
-
-tryInsertTagQ :: V.Vector T.Text -> App (V.Vector D1.Statement)
-tryInsertTagQ = do
-  prepareBinds "INSERT OR IGNORE INTO tags (name) VALUES (?)"
-    . fmap (V.singleton . D1.toD1ValueView)
-
-lookupTagsQ :: V.Vector T.Text -> App (V.Vector D1.Statement)
-lookupTagsQ =
-  prepareBinds "SELECT * FROM tags WHERE name = ?"
-    . fmap (V.singleton . D1.toD1ValueView)
-
-articleTagsQ :: ArticleId -> App D1.Statement
-articleTagsQ =
-  prepareBind "SELECT tag.name FROM tags tag INNER JOIN articleTags assoc ON tag.id = assoc.tag WHERE assoc.article = ?"
-    . V.singleton
-    . D1.toD1ValueView
-
-insertArticleQ :: ArticleSeed -> App D1.Statement
-insertArticleQ ArticleSeed {..} = do
-  now <- liftIO getCurrentTime
-  prepareBind "INSERT INTO articles (body, createdAt, lastUpdate, slug) VALUES (?1, ?2, ?3, ?4)" $
-    V.fromList
-      [ D1.toD1ValueView body
-      , D1.toD1ValueView now
-      , D1.toD1ValueView now
-      , D1.toD1ValueView slug
-      ]
-
-tagArticleQ :: V.Vector TagId -> ArticleId -> App (V.Vector D1.Statement)
-tagArticleQ tids aid = do
-  prepareBinds "INSERT INTO articleTags (article, tag) VALUES (?1, ?2) ON CONFLICT DO NOTHING" $
-    V.map (\tid -> V.fromList [D1.toD1ValueView aid, D1.toD1ValueView tid]) tids
-
-articleWithTagsQ :: T.Text -> Word32 -> App D1.Statement
-articleWithTagsQ name page = do
-  prepareBind "SELECT a.* FROM articles a INNER JOIN articleTags at ON a.id = at.article INNER JOIN tags t ON at.tag = t.id WHERE t.name = ?1 ORDER BY a.createdAt DESC LIMIT 10 OFFSET ?2" $
-    V.fromList [D1.toD1ValueView name, D1.toD1ValueView $ page * 10]
-
-deleteAllTagsOnArticleQ :: ArticleId -> App D1.Statement
-deleteAllTagsOnArticleQ art = do
-  prepareBind "DELETE FROM articleTags WHERE article = ?" $
-    V.singleton $
-      D1.toD1ValueView art
-
-deleteArticleQ :: ArticleId -> App D1.Statement
-deleteArticleQ aid = do
-  prepareBind "DELETE FROM articles WHERE id = ?" $
-    V.singleton $
-      D1.toD1ValueView aid
-
 fromArticleRow ::
   ArticleRow ->
   App Article
 fromArticleRow arow = do
-  rows <- liftIO . (wait <=< D1.all) =<< articleTagsQ arow.id
+  artTagsQ <- mkArticleTagsQ
+  rows <- liftIO . (wait <=< D1.all) =<< bind artTagsQ arow.id
   unless rows.success $
     throwString "Failed to fetch tags for article"
   let (fails, tags) = partitionEithers $ map (fmap getTagName . D1.parseD1RowView) $ V.toList rows.results
