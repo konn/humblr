e4dca396d12d9d0ec648552d3f752c3ba8d9d74b
Hiromi ISHII <konn.jinro@gmail.com>
2024-11-02 11:49:26 +0900

    Now image service works!

--- humblr-workers/src/Humblr/Worker/Images.hs
+++ humblr-workers/src/Humblr/Worker/Images.hs
@@ -25,22 +25,18 @@ import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Maybe
 import Data.Aeson qualified as A
 import Data.Char qualified as C
-import Data.Functor ((<&>))
-import Data.Maybe (catMaybes, isNothing)
-import Data.String (IsString (..))
 import Data.Text qualified as T
 import GHC.Generics
 import GHC.Wasm.Object.Builtins
-import GHC.Wasm.Web.Generated.Response qualified as RawResp
+import GHC.Wasm.Web.JSON (encodeJSON)
 import Humblr.Worker.Storage (SignParams (..), StorageServiceClass)
 import Network.Cloudflare.Worker.Binding (BindingsClass)
 import Network.Cloudflare.Worker.Binding.Service
 import Network.Cloudflare.Worker.FetchAPI (fetch)
-import Network.Cloudflare.Worker.Response (WorkerResponse, newResponse')
+import Network.Cloudflare.Worker.Response (WorkerResponse)
 import Servant.Cloudflare.Workers (err404)
 import Servant.Cloudflare.Workers.Internal.Response (toWorkerResponse)
 import Servant.Cloudflare.Workers.Internal.ServerError (responseServerError)
-import Wasm.Prelude.Linear qualified as PL
 
 data ImagesServiceFuns = ImagesServiceFuns
   {thumb, medium, large :: T.Text -> App (Maybe WorkerResponse)}
@@ -87,18 +83,6 @@ instance A.ToJSON ImageMetadata where
 data ImageOption = ImageOption {height, width :: !(Maybe Word), metadata :: !(Maybe ImageMetadata), fit :: !(Maybe Fit)}
   deriving (Show, Eq, Ord, Generic)
 
-commaSep :: ImageOption -> T.Text
-commaSep ImageOption {..} =
-  T.intercalate "," $
-    catMaybes
-      [ ("height=" <>) . T.pack . show <$> height
-      , ("width=" <>) . T.pack . show <$> width
-      , metadata <&> \mt ->
-          "metadata=" <> T.toLower (T.pack $ show mt)
-      , fit <&> \f ->
-          "fit=" <> T.pack (A.camelTo2 '-' $ show f)
-      ]
-
 instance A.ToJSON ImageOption where
   toJSON = A.genericToJSON A.defaultOptions {A.omitNothingFields = True}
 
@@ -136,38 +120,10 @@ withImageOptions :: ImageOption -> T.Text -> App (Maybe WorkerResponse)
 withImageOptions opts path
   | T.null path = liftIO $ fmap Just $ toWorkerResponse $ responseServerError err404
   | otherwise = do
-      liftIO $ consoleLog $ fromText $ "withImageOptions: " <> T.pack (show (opts, path))
       storage <- getBinding "STORAGE"
-      root <- getEnv "ROOT_URI"
       liftIO $ runMaybeT do
         url <- MaybeT $ await' =<< storage.issueSignedURL SignParams {duration = 60, name = path}
-        liftIO $ consoleLog $ fromString $ "Signed URL: " <> show url
         liftIO do
-          let cdn =
-                T.intercalate
-                  "/"
-                  [ T.dropWhileEnd (== '/') root
-                  , "cdn-cgi/image"
-                  , commaSep opts
-                  , url
-                  ]
-          consoleLog $ fromString $ "CDN URL: " <> show cdn
-          resp <- await =<< fetch (inject $ fromText @USVStringClass cdn) none
-          body <- RawResp.js_get_body resp
-          consoleLog $ fromString $ "Body is none?: " <> show (isNothing $ fromNullable body)
-          stat <- RawResp.js_get_status resp
-          statTxt <- RawResp.js_get_statusText resp
-          headers <- RawResp.js_get_headers resp
-          encode <- fromHaskellByteString "manual"
-          cf <- emptyObject
-          newResponse' (inject <$> fromNullable body) $
-            Just $
-              newDictionary
-                PL.$ setPartialField "status" (toJSPrim stat)
-                PL.. setPartialField "statusText" statTxt
-                PL.. setPartialField "headers" (inject headers)
-                PL.. setPartialField "encodeBody" encode
-                PL.. setPartialField "cf" cf
-
-foreign import javascript unsafe "console.log($1)"
-  consoleLog :: USVString -> IO ()
+          cf <- encodeJSON $ A.object ["cf" A..= A.object ["image" A..= opts]]
+          fmap unsafeCast . await
+            =<< fetch (inject $ fromText @USVStringClass url) (nonNull $ unsafeCast cf)
