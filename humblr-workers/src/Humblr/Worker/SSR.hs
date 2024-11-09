6c3b839e49d4eb8163fe673035103d986fb05412
Hiromi ISHII <konn.jinro@gmail.com>
2024-11-09 16:18:41 +0900

    Missing content-type!

--- humblr-workers/src/Humblr/Worker/SSR.hs
+++ humblr-workers/src/Humblr/Worker/SSR.hs
@@ -110,7 +110,8 @@ renderArticle slug = do
               link_ [rel_ "stylesheet", type_ "text/css", href_ "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"]
               script_ mempty `with` [src_ script, type_ "module", async_ "", defer_ ""]
 
-              meta_ [name_ "twitter:card", content_ "summary_large_image"]
+              meta_ [name_ "twitter:card", content_ "summary"]
+              meta_ [name_ "twitter:site", content_ "@mr_konn"]
 
               meta_ [property_ "og:type", content_ "website"]
               meta_ [property_ "og:url", content_ $ linkWithRoot root $ rootApiLinks.frontend.articlePage slug]
@@ -131,7 +132,7 @@ renderArticle slug = do
         { status = 200
         , body = Just $ Resp.WorkerResponseLBS body
         , statusText = "Ok"
-        , headers = M.fromList [("Last-Modified", BS8.pack $ formatTime defaultTimeLocale rfc822DateFormat $ art.updatedAt)]
+        , headers = M.fromList [("Last-Modified", BS8.pack $ formatTime defaultTimeLocale rfc822DateFormat $ art.updatedAt), ("Content-Type", "text/html; charset=utf-8")]
         }
 
 linkWithRoot :: T.Text -> Link -> T.Text
